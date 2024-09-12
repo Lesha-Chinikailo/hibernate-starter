69178e3a942d7e3789b5d867f6b686f57aa9b858
Lesha-Chinikailo <lesha2004.0000@gmail.com>
2024-09-12 22:43:58 +0300

    init commit

---
+++ src/main/java/com/dmdev/HibernateRunner.java
@@ -0,0 +1,32 @@
+package com.dmdev;
+
+import com.dmdev.entity.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.time.LocalDate;
+
+public class HibernateRunner {
+
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration();
+//        configuration.addAnnotatedClass(User.class);
+        configuration.configure();
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
+             Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+
+            User user = User.builder()
+                    .username("ivan@gmail.com")
+                    .firstname("Ivan")
+                    .lastname("Ivanov")
+                    .birthDate(LocalDate.of(2000, 1, 20))
+                    .age(24)
+                    .build();
+            session.persist(user);
+
+            session.getTransaction().commit();
+        }
+    }
+}
