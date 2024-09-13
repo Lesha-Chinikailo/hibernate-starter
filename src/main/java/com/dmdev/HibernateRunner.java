897d3bf82662e6cad2d97b8ff465e9e14ffcc74b
Lesha-Chinikailo <lesha2004.0000@gmail.com>
2024-09-13 23:19:12 +0300

    add BirthdayConverter

--- src/main/java/com/dmdev/HibernateRunner.java
+++ src/main/java/com/dmdev/HibernateRunner.java
@@ -1,5 +1,8 @@
 package com.dmdev;
 
+import com.dmdev.converter.BirthdayConverter;
+import com.dmdev.entity.Birthday;
+import com.dmdev.entity.Role;
 import com.dmdev.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -12,17 +15,18 @@ public class HibernateRunner {
     public static void main(String[] args) {
         Configuration configuration = new Configuration();
 //        configuration.addAnnotatedClass(User.class);
+        configuration.addAttributeConverter(new BirthdayConverter());
         configuration.configure();
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.openSession()) {
             session.beginTransaction();
 
             User user = User.builder()
-                    .username("ivan@gmail.com")
+                    .username("ivan1@gmail.com")
                     .firstname("Ivan")
                     .lastname("Ivanov")
-                    .birthDate(LocalDate.of(2000, 1, 20))
-                    .age(24)
+                    .birthDate(new Birthday(LocalDate.of(2000, 1, 20)))
+                    .role(Role.ADMIN)
                     .build();
             session.persist(user);
 
