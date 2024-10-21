d0c0f638c24dec46094c74817057f4ca1606344a
Lesha-Chinikailo <lesha2004.0000@gmail.com>
2024-10-21 12:46:58 +0300

    learn graph and subgraph

--- src/main/java/com/dmdev/HibernateRunner.java
+++ src/main/java/com/dmdev/HibernateRunner.java
@@ -9,11 +9,15 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.graph.GraphSemantic;
+import org.hibernate.graph.RootGraph;
+import org.hibernate.graph.SubGraph;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 public class HibernateRunner {
@@ -24,14 +28,26 @@ public class HibernateRunner {
         Session session = sessionFactory.openSession()){
             TestDataImporter.importData(sessionFactory);
             session.beginTransaction();
+            RootGraph<User> userGraph = session.createEntityGraph(User.class);
+            userGraph.addAttributeNodes("company", "userChats");
+            SubGraph<UserChat> userChatsSubgraph = userGraph.addSubgraph("userChats", UserChat.class);
+            userChatsSubgraph.addAttributeNodes("chat");
+
+            Map<String, Object> properties = Map.of(
+//                    GraphSemantic.LOAD.getJpaHintName(), session.getEntityGraph("WithCompanyAndChat")
+                    GraphSemantic.LOAD.getJpaHintName(), userGraph
+            );
+            User user = session.find(User.class, 1L, properties);
+            System.out.println(user.getCompany().getName());
+            System.out.println(user.getUserChats().size());
 
             List<User> users = session.createQuery(
-                            "select u from User u " +
-                                    "join fetch u.payments p " +
-                                    "join fetch u.company c ", User.class)
+                            "select u from User u", User.class)
+                    .setHint(GraphSemantic.LOAD.getJpaHintName(), session.getEntityGraph("WithCompanyAndChat"))
+                    .setHint(GraphSemantic.LOAD.getJpaHintName(), userGraph)
                     .list();
-            users.forEach(user -> System.out.println(user.getPayments().size()));
-            users.forEach(user -> System.out.println(user.getCompany().getName()));
+            users.forEach(it -> System.out.println(it.getUserChats().size()));
+            users.forEach(it -> System.out.println(it.getCompany().getName()));
 
             session.getTransaction().commit();
 
--- src/main/java/com/dmdev/entity/User.java
+++ src/main/java/com/dmdev/entity/User.java
@@ -10,26 +10,22 @@ import lombok.ToString;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
 
-import javax.persistence.AttributeOverride;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 import static com.dmdev.util.StringsUtils.SPACE;
 
+@NamedEntityGraph(
+        name = "WithCompanyAndChat",
+        attributeNodes = {
+                @NamedAttributeNode("company"),
+                @NamedAttributeNode(value = "userChats", subgraph = "chats")
+        },
+        subgraphs = {
+            @NamedSubgraph(name = "chats", attributeNodes = @NamedAttributeNode("chat"))
+        }
+)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
